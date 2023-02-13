1434a5b9fa8e2aac35bd665d8fdc060df6c68b25
Milton Figueredo Miles <gratno@gmail.com>
2023-02-13 12:35:26 -0500

    Datos numericos, enteros - decimales - boolean - string - datos primtivos

---
+++ src/com/example/Tipos.java
@@ -0,0 +1,49 @@
+package com.example;
+
+/*** JAVA DOC
+ *
+ *
+ * @Autor Juanita Alimaña
+ *
+ * @date  Febrero
+ *
+ * Crear método Main
+ * datos primitivos
+ */
+
+public class Tipos {
+
+    public static void main(String[] args) {
+
+
+        //Los datos primitivos  no pueden ser Null
+        //1. numericos
+
+        //1-1 Enteros
+        byte variable1 = 5;
+        short varieble2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+        //la diferencia entre ellos es la capacidad de guardar info
+
+
+        //1.2 Decimales
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+
+
+        //2. booleanos
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        //Datos primitivos
+
+
+        //3. texto
+        char variable9 = 'a';
+
+        //STRING es una CLASE
+        String variable10 = "Hola"; // es una CLASE
+
+    }
+}
